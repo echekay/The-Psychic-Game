c5d7187e74822b2671fee9b221fd4cd8e471727d
Hongsyn Keath <Hongsyn Keath>
2017-06-28 19:41:37 -0400

    A little bit of this, a little bit of that.

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// alert("Javascript file is linked!")
+
+var alphabet:Array = ("abcdefghijklmnopqrstuvwxyz").split("");
+
+if (var i=0; i<alphabet.length; i++) {
+
+}
+var userGuess = 
+var userGuessLower = userGuess.toLowerCase();
+
+if (alphabet.indexOf(userGuessLower) === -1) {
+	userLosses++;
+}
+else {
+	userWins++;
+}
