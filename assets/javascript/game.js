b89e346baeb7437b7632c10242bfe07de7ff20a2
Hongsyn Keath <Hongsyn Keath>
2017-06-28 21:12:12 -0400

    Code almost done, for loop logic incorrect, need to debug.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,37 @@
-// alert("Javascript file is linked!")
+// var alphabet:Array = ("abcdefghijklmnopqrstuvwxyz").split("");
 
-var alphabet:Array = ("abcdefghijklmnopqrstuvwxyz").split("");
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lettersGuessed = [];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
 
-if (var i=0; i<alphabet.length; i++) {
+document.onkeyup = function(event) {
 
+    var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    var userGuess = event.key;
+    var userGuessLower = userGuess.toLowerCase();
+
+
+    for (var i = 0; i < alphabet.length; i++) {
+        if (userGuessLower === alphabet[i]) {
+            wins++;
+        } else {
+            losses++;
+            guessesLeft--;
+
+        }
+        lettersGuessed.push(userGuessLower);
+    }
+
+    var html =
+        "<h1>The Psychic Game</h1>" +
+        "<p>Guess what letter I'm thinking of.</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses left: " + guessesLeft + "</p>" + 
+        "<p>Letters Guessed: " + lettersGuessed + "</p>";
+
+    document.querySelector("#psychic").innerHTML = html;
 }
-var userGuess = 
-var userGuessLower = userGuess.toLowerCase();
 
-if (alphabet.indexOf(userGuessLower) === -1) {
-	userLosses++;
-}
-else {
-	userWins++;
-}
